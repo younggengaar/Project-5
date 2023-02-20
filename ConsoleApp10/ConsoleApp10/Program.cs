8724f63d568b214cc9f56d5e35a35468d6e6dfc4
liza <123323177+younggengaar@users.noreply.github.com>
2023-02-20 23:19:37 +0300

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //задание1
+            //int[] omas = new int[6];
+            //Random rnd = new Random();
+            //for ( int i=0; i<=5; i++)
+            //{
+            // omas[i] = rnd.Next(-6, 6);
+            // Console.WriteLine($"omas[{i}]={omas[i]}\n");
+            //}
+            //Console.WriteLine();
+            //for(int i=0; i<=5; i++)
+            //{
+            // if (omas[i] == omas[1])
+            // {
+            // Console.WriteLine($"i={omas[1]}");
+            // }
+            //}
+            //Console.ReadKey();
+
+            //задание2
+            //int[] omas = new int[] { 1, 3, 5, -3, -9, -11, 2 };
+            //Console.WriteLine("Неотрицательные числа:");
+            //for (int i=0; i<omas.Length; i++)
+            //{
+            // if (omas[i] > 0)
+            // {
+            // Console.WriteLine($"omas[{i}]={omas[i]}");
+            // }
+            //}
+            //Console.WriteLine("Элементы, не превышающие 100:");
+            //for (int i=0;i<omas.Length; i++)
+            //{
+            // if (omas[i] <= 100)
+            // {
+            // Console.WriteLine($"omas[{i}]={omas[i]}");
+            // }
+            // if (omas[i] % 2 == 0)
+            // {
+            // Console.WriteLine($" omas[{i}]= {omas[i]}");
+            // }
+            //}
+            //Console.ReadKey();
+
+            //задание3
+            //int[] omas = new int[] { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+            //byte count = 0;
+            //for (int i=0; i<omas.Length; i++)
+            //{
+            // Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //for(int i =0; i<omas.Length; i++)
+            //{
+            // if (omas[i] < 0)
+            // {
+            // count++;
+            // }
+            //}
+            //Console.WriteLine($"count={count}");
+            //Console.ReadKey();
+
+            //задание4
+            //int[] omas = new int[] { 7, 10, 34, 8 };
+            //float sr_ar = 0;
+            //for (int i = 0; i <= 3; i++)
+            //{
+            //    sr_ar += omas[i];
+            //}
+            //sr_ar /= 4;
+            //Console.WriteLine("Среднее арифмитическое={0}", sr_ar);
+            //Console.ReadKey();
+
+        }
+    }
+}
